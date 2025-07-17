837bf15eba314b4397d68b2690abc6fde460301f
Tran-Anh-Tuan11 <trantuan1112t@gmail.com>
2025-07-17 23:53:15 +0700

    test git action file

--- src/test/java/com/vpbankhackathon/transaction_monitoring/TransactionMonitoringApplicationTests.java
+++ src/test/java/com/vpbankhackathon/transaction_monitoring/TransactionMonitoringApplicationTests.java
@@ -2,7 +2,7 @@ package com.vpbankhackathon.transaction_monitoring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test github actions
 @SpringBootTest
 class TransactionMonitoringApplicationTests {
 
