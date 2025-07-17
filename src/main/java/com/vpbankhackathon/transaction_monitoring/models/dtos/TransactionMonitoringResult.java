1630873865e0874419a230bdfde5789bc5472fc5
thinh205171 <thinhprodtrai@gmail.com>
2025-07-17 21:55:32 +0700

    fix: NamedQuery spring data

--- src/main/java/com/vpbankhackathon/transaction_monitoring/models/dtos/TransactionMonitoringResult.java
+++ src/main/java/com/vpbankhackathon/transaction_monitoring/models/dtos/TransactionMonitoringResult.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 @Data
 public class TransactionMonitoringResult {
+
     private Long transactionId;
     private String status; // CLEAR or SUSPENDED or SUSPICIOUS
     private String reason;
